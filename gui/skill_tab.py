b9d2bf28c147a038f34f10fcaf3067c5a9f83058
shikinora2 <dang2000123@gmail.com>
2025-07-21 14:31:01 +0700

    Update skill_tab.py

--- gui/skill_tab.py
+++ gui/skill_tab.py
@@ -1,4 +1,6 @@
 ﻿# gui/skill_tab.py
+# --- SỬA LỖI ---
+# Chuyển sang sử dụng import tương đối (relative import)
 from .base_image_condition_tab import BaseImageConditionTab
 
 class SkillTab(BaseImageConditionTab):
@@ -7,5 +9,4 @@ class SkillTab(BaseImageConditionTab):
     Kế thừa toàn bộ giao diện và logic từ BaseImageConditionTab.
     """
     def __init__(self, master, app, **kwargs):
-        # Chỉ cần gọi lớp cha và truyền vào tên của tab này là "Quy tắc Skill"
         super().__init__(master, app, tab_name="Quy tắc Skill", **kwargs)
